52a5590b63c9e5ac50f0c142bc3d41afde21e1d8
Akash-Ramdas <akashramdas97@gmail.com>
2021-02-25 08:16:35 -0800

    Commit 2

--- akash_mastery_nope.py
+++ akash_mastery_nope.py
@@ -1,1 +1,17 @@
-print("Hello")
+print("Hi")
+
+import os
+
+filename = "output.txt"
+with open(filename, "w") as f:
+    f.write("Output \n")
+
+out_dir_name = "Output_dir"
+curr_dir = os.getcwd()
+out_dir = curr_dir + "/" + out_dir_name
+os.makedirs(out_dir, exist_ok=True)
+os.chdir(out_dir)
+
+filename = "output.txt"
+with open(filename, "w") as f:
+    f.write("Output \n")
